993c4ced611bcf35bf728d6c805a2b84099c148a
Jubril Siji <oyesiji@gmail.com>
2020-08-18 21:30:16 -0400

    Made changes

--- src/main/java/bootwildfly/HelloWildFlyController.java
+++ src/main/java/bootwildfly/HelloWildFlyController.java
@@ -1,11 +1,17 @@
 package bootwildfly;
 
 import java.util.Date;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.abbey.UserRepository;
+import com.abbey.entity.User;
 
 @RestController
 public class HelloWildFlyController {
+
+  @Autowired
+  private UserRepository userRepository;
 
 
   @RequestMapping("hello")
@@ -25,4 +31,18 @@ public class HelloWildFlyController {
   public String saySeun() {
     return ("Hello,Seun" + new Date().toString());
   }
+
+
+  @RequestMapping("save")
+  public String save() {
+
+    User n = new User();
+    n.setName("Jubril Siji");
+    n.setEmail("oyesiji@gmail.com");
+    userRepository.save(n);
+    return "Saved";
+  }
+
+
+
 }
