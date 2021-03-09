e77ed1f83691eb0a379ff529e08d99426d14c5a6
Jubril Siji <oyesiji@gmail.com>
2021-03-08 21:45:27 -0500

    MAde chnages

--- src/main/java/com/abbey/HelloWildFlyController.java
+++ src/main/java/com/abbey/HelloWildFlyController.java
@@ -1,48 +1,44 @@
 package com.abbey;
 
 import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.abbey.entity.User;
 import com.abbey.repo.UserRepository;
 
 @RestController
 public class HelloWildFlyController {
 
-  @Autowired
-  private UserRepository userRepository;
+	@Autowired
+	private UserRepository userRepository;
 
+	@RequestMapping("hello")
+	public String sayHello() {
+		String prefix = System.getenv().getOrDefault("greeting_prefix", "Hi");
+		return ("Hello, SpringBoot on Wildfly 1988" + prefix);
+	}
 
-  @RequestMapping("hello")
-  public String sayHello() {
-    return ("Hello, SpringBoot on Wildfly 1988");
-  }
+	@RequestMapping("hi")
+	public String sayHi() {
+		return ("Hello, SpringBoot on Wildfly 1988, say Hi" + new Date().toString());
+	}
 
+	@RequestMapping("seun")
+	public String saySeun() {
+		return ("Hello,Seun" + new Date().toString());
+	}
 
-  @RequestMapping("hi")
-  public String sayHi() {
-    return ("Hello, SpringBoot on Wildfly 1988, say Hi" + new Date().toString());
-  }
+	@RequestMapping("save")
+	public String save() {
 
-
-
-  @RequestMapping("seun")
-  public String saySeun() {
-    return ("Hello,Seun" + new Date().toString());
-  }
-
-
-  @RequestMapping("save")
-  public String save() {
-
-    User n = new User();
-    n.setName("Jubril Siji");
-    n.setEmail("oyesiji@gmail.com");
-    userRepository.save(n);
-    return "Saved";
-  }
-
-
+		User n = new User();
+		n.setName("Jubril Siji");
+		n.setEmail("oyesiji@gmail.com");
+		userRepository.save(n);
+		return "Saved";
+	}
 
 }
